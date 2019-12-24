76324f4f793362f9b796a0d3252da1540b963c2e
Antonio Mata <hildeamp@gmail.com>
2019-12-23 21:52:49 -0600

    :recycle: Import Auth service

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { JwtStrategy } from './jwt.strategy';
 import { ConfigService } from '@nestjs/config';
+import { AuthService } from './auth.service';
 
 @Module({
   providers: [AuthService, JwtStrategy],
