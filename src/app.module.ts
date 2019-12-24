1db135afcf4d1a67cd342f0de411bd6c2e3e680c
Antonio Mata <hildeamp@gmail.com>
2019-12-23 21:53:45 -0600

    :recycle: Refactor app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,9 @@ import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { LoginModule } from './login/login.module';
 import { JwtModule } from '@nestjs/jwt';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 
 @Module({
   imports: [
@@ -20,9 +23,28 @@ import { JwtModule } from '@nestjs/jwt';
         signOptions: { expiresIn: '1h' },
       }),
     }),
+    UserModule,
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        return {
+          type: 'postgres' as 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get<number>('DB_PORT'),
+          database: configService.get('DB_NAME'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASS'),
+          entities: ['dist/**/*.entity{.ts,.js}'],
+          synchronize: true,
+        } as TypeOrmModuleOptions;
+      },
+      inject: [ConfigService],
+    }),
   ],
 })
 export class AppModule implements NestModule {
+  constructor(private readonly connection: Connection) {}
+
   configure(consumer: MiddlewareConsumer): void {
     consumer.apply(ValidateSearchMiddleware, ValidateSourceParamMiddleware).forRoutes('news');
     consumer.apply(ValidateJsonMiddleware).forRoutes('login');
