e98fbb24e29e987ba482533e660dda9f411e2ef8
Antonio Mata <hildeamp@gmail.com>
2019-12-24 15:18:29 -0600

    :sparkles: Add create method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './user.entity';
 import { Repository } from 'typeorm';
+import { CreateUserDto } from './dto/user.dto';
 
 @Injectable()
 export class UserService {
@@ -9,5 +10,9 @@ export class UserService {
 
   findAll(): Promise<User[]> {
     return this.userRepository.find();
+  }
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    return this.userRepository.create(createUserDto);
   }
 }
