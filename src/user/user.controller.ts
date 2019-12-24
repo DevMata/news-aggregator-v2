8cb7cfa0040a8bf2aa291757b21e1c2c9ab47114
Antonio Mata <hildeamp@gmail.com>
2019-12-23 21:52:02 -0600

    :sparkles: Add findAll method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { User } from './user.entity';
+import { UserService } from './user.service';
 
-@Injectable()
-export class UserController {}
+@Controller('users')
+export class UserController {
+  constructor(private readonly userService: UserService) {}
+
+  @Get()
+  findAll(): Promise<User[]> {
+    return this.userService.findAll();
+  }
+}
