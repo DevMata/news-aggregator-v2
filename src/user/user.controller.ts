9e41c4ed12e5c00866ad775f83e0ee1bf3ab0f3e
Antonio Mata <hildeamp@gmail.com>
2019-12-24 15:18:36 -0600

    :sparkles: Add create method

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, UsePipes, ValidationPipe, Body } from '@nestjs/common';
 import { User } from './user.entity';
 import { UserService } from './user.service';
+import { CreateUserDto } from './dto/user.dto';
 
 @Controller('users')
 export class UserController {
@@ -9,5 +10,14 @@ export class UserController {
   @Get()
   findAll(): Promise<User[]> {
     return this.userService.findAll();
+  }
+
+  @Post()
+  @UsePipes(new ValidationPipe({ transform: true }))
+  createOne(
+    @Body()
+    createUserDto: CreateUserDto,
+  ): Promise<User> {
+    return this.userService.createUser(createUserDto);
   }
 }
