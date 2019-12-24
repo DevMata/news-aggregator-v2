99436d76e2fe4ea6db166af215ebf6c065c53bf4
Antonio Mata <hildeamp@gmail.com>
2019-12-23 18:34:42 -0600

    Add JwtModule to AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,25 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { ValidateSearchMiddleware } from './news/middleware/validate-search.middleware';
 import { ValidateSourceParamMiddleware } from './news/middleware/validate-source-param.middleware';
 import { ValidateJsonMiddleware } from './login/middleware/validate-json.middleware';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { NewsModule } from './news/news.module';
 import { LoginModule } from './login/login.module';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), NewsModule, LoginModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    NewsModule,
+    LoginModule,
+    JwtModule.registerAsync({
+      imports: [ConfigService],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer): void {
