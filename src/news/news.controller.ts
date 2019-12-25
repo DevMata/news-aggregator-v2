2ebad3b8c19bfc2ac239ccab423fc4b57efacb9c
Antonio Mata <hildeamp@gmail.com>
2019-12-25 16:01:23 -0600

    :recycle: Remove use guards anotation

--- src/news/news.controller.ts
+++ src/news/news.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { NewsService } from './news.service';
-import { ValidateAccessGuard } from './guards/validate-access.guard';
 import { Observable } from 'rxjs';
 import { New } from './interfaces/New.interface';
 
@@ -9,7 +8,6 @@ export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
   @Get()
-  @UseGuards(ValidateAccessGuard)
   findAll(@Query('q') q: string, @Query('source') source: string): Observable<New[]> {
     return this.newsService.switchSourceSearch(q, source);
   }
