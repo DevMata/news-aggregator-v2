6b4638ad13196f84cad39427402b59afd2e7e521
Antonio Mata <hildeamp@gmail.com>
2019-12-25 15:46:48 -0600

    :recycle: Remove constants from service

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -2,15 +2,9 @@ import { Injectable, HttpService, BadRequestException, ServiceUnavailableExcepti
 import { ConfigService } from '@nestjs/config';
 import { Observable, merge } from 'rxjs';
 import { map, reduce } from 'rxjs/operators';
-
 import { SerializeNewYorkNew, SerializeGuardianNew, SerializeNewsApiNew } from './helpers/serialize.helper';
 import { New } from './interfaces/New.interface';
-
-const NYT_URL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?';
-const NYT_FILTERS = '&sort=relevance&fl=_id,pub_date,snippet,web_url,headline,byline';
-const GUARDIAN_URL = 'https://content.guardianapis.com/search';
-const GUARDIAN_FILTERS = '&show-tags=contributor';
-const NEWS_URL = 'https://newsapi.org/v2/everything?';
+import { NewsConstants as cts } from './news.constants';
 
 @Injectable()
 export class NewsService {
@@ -40,7 +34,7 @@ export class NewsService {
       const NYT_KEY = this.configService.get<string>('NYT_KEY');
 
       return this.httpService
-        .get(`${NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${NYT_FILTERS}`)
+        .get(`${cts.NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${cts.NYT_FILTERS}`)
         .pipe(map(res => res.data.response.docs.map(SerializeNewYorkNew)));
     } catch {
       throw new ServiceUnavailableException('NYT does not respond');
@@ -52,7 +46,7 @@ export class NewsService {
       const GUARDIAN_KEY = this.configService.get<string>('GUARDIAN_KEY');
 
       return this.httpService
-        .get(`${GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${GUARDIAN_FILTERS}`)
+        .get(`${cts.GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${cts.GUARDIAN_FILTERS}`)
         .pipe(map(res => res.data.response.results.map(SerializeGuardianNew)));
     } catch {
       throw new ServiceUnavailableException('Guardian does not respond');
@@ -64,7 +58,7 @@ export class NewsService {
       const NEWS_KEY = this.configService.get<string>('NEWS_KEY');
 
       return this.httpService
-        .get(`${NEWS_URL}apiKey=${NEWS_KEY}&q=${searchTerm}`)
+        .get(`${cts.NEWS_URL}apiKey=${NEWS_KEY}&q=${searchTerm}`)
         .pipe(map(res => res.data.articles.map(SerializeNewsApiNew)));
     } catch {
       throw new ServiceUnavailableException('NewsAPI does not respond');
