fea62ec91ac76d3638d68476076f83c11071d773
Antonio Mata <hildeamp@gmail.com>
2019-12-26 12:36:55 -0600

    :recycle: Add catchError handling in axios requests

--- src/news/news.service.ts
+++ src/news/news.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, HttpService, BadRequestException, ServiceUnavailableException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { Observable, merge } from 'rxjs';
-import { map, reduce } from 'rxjs/operators';
+import { Observable, merge, throwError } from 'rxjs';
+import { map, reduce, catchError } from 'rxjs/operators';
 import { SerializeNewYorkNew, SerializeGuardianNew, SerializeNewsApiNew } from './helpers/serialize.helper';
 import { New } from './interfaces/New.interface';
 import { NewsConstants as cts } from './news.constants';
@@ -30,39 +30,38 @@ export class NewsService {
   }
 
   private newYorkTimeSearch(searchTerm: string): Observable<New[]> {
-    try {
-      const NYT_KEY = this.configService.get<string>('NYT_KEY');
+    const NYT_KEY = this.configService.get<string>('NYT_KEY');
 
-      return this.httpService
-        .get(`${cts.NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${cts.NYT_FILTERS}`)
-        .pipe(map(res => res.data.response.docs.map(SerializeNewYorkNew)));
-    } catch {
-      throw new ServiceUnavailableException('NYT does not respond');
-    }
+    return this.httpService.get(`${cts.NYT_URL}q=${searchTerm}&api-key=${NYT_KEY}${cts.NYT_FILTERS}`).pipe(
+      map(res => res.data.response.docs.map(SerializeNewYorkNew)),
+      catchError(() => {
+        return throwError(new ServiceUnavailableException('News API does not respond'));
+      }),
+    );
   }
 
   private guardianSearch(searchTerm: string): Observable<New[]> {
-    try {
-      const GUARDIAN_KEY = this.configService.get<string>('GUARDIAN_KEY');
+    const GUARDIAN_KEY = this.configService.get<string>('GUARDIAN_KEY');
 
-      return this.httpService
-        .get(`${cts.GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${cts.GUARDIAN_FILTERS}`)
-        .pipe(map(res => res.data.response.results.map(SerializeGuardianNew)));
-    } catch {
-      throw new ServiceUnavailableException('Guardian does not respond');
-    }
+    return this.httpService
+      .get(`${cts.GUARDIAN_URL}?api-key=${GUARDIAN_KEY}&q=${searchTerm}${cts.GUARDIAN_FILTERS}`)
+      .pipe(
+        map(res => res.data.response.results.map(SerializeGuardianNew)),
+        catchError(() => {
+          return throwError(new ServiceUnavailableException('News API does not respond'));
+        }),
+      );
   }
 
   private newsApiSearch(searchTerm: string): Observable<New[]> {
-    try {
-      const NEWS_KEY = this.configService.get<string>('NEWS_KEY');
+    const NEWS_KEY = this.configService.get<string>('NEWS_KEY');
 
-      return this.httpService
-        .get(`${cts.NEWS_URL}apiKey=${NEWS_KEY}&q=${searchTerm}`)
-        .pipe(map(res => res.data.articles.map(SerializeNewsApiNew)));
-    } catch {
-      throw new ServiceUnavailableException('NewsAPI does not respond');
-    }
+    return this.httpService.get(`${cts.NEWS_URL}apiKey=${NEWS_KEY}&q=${searchTerm}`).pipe(
+      map(res => res.data.articles.map(SerializeNewsApiNew)),
+      catchError(() => {
+        return throwError(new ServiceUnavailableException('News API does not respond'));
+      }),
+    );
   }
 
   private allSearch(searchTerm: string): Observable<New[]> {
