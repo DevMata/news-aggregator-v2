50a20af763011027b6a55462704c3572f636e077
Antonio Mata <hildeamp@gmail.com>
2019-12-26 17:17:30 -0600

    :recycle: Refactor UsersToNews entity

--- src/users/userstonews/userstonews.entity.ts
+++ src/users/userstonews/userstonews.entity.ts
@@ -1,26 +1,23 @@
-import { Entity, PrimaryColumn, CreateDateColumn, ManyToOne } from 'typeorm';
+import { Entity, CreateDateColumn, ManyToOne, PrimaryGeneratedColumn } from 'typeorm';
 import { New } from '../news/news.entity';
 import { User } from '../user.entity';
 
 @Entity()
 export class UsersToNews {
-  @PrimaryColumn()
-  userId: string;
-
-  @PrimaryColumn()
-  newId: string;
+  @PrimaryGeneratedColumn('uuid')
+  usersToNewsId: string;
 
   @CreateDateColumn()
   savedAt: Date;
 
   @ManyToOne(
-    type => User,
+    () => User,
     user => user.usersToNews,
   )
   public user!: User;
 
   @ManyToOne(
-    type => New,
+    () => New,
     n => n.usersToNews,
   )
   public new!: New;
