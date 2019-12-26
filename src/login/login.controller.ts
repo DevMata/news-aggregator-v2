a13abff69a87eeb686e0b410094d414cfcd8c398
Antonio Mata <hildeamp@gmail.com>
2019-12-25 22:15:07 -0600

    :sparkles: Add method for try login

--- src/login/login.controller.ts
+++ src/login/login.controller.ts
@@ -1,7 +1,8 @@
-import { Controller, Post, Body, UsePipes, BadRequestException } from '@nestjs/common';
+import { Controller, Post, Body, UsePipes, BadRequestException, Request, UseGuards } from '@nestjs/common';
 import { ValidateLoginPayloadPipe } from './pipes/validate-login-payload.pipe';
 import { LoginDto } from './dto/login.dto';
 import { LoginService } from './login.service';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('login')
 export class LoginController {
@@ -16,5 +17,11 @@ export class LoginController {
     } else {
       throw new BadRequestException('User not found');
     }
+  }
+
+  @Post('auth')
+  @UseGuards(AuthGuard('local'))
+  async login(@Request() req): Promise<{ accessToken: string }> {
+    return this.loginService.login(req.user);
   }
 }
