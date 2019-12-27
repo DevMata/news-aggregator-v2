c67fe16a67ec73c5102a8101dc5482d643c091d0
Antonio Mata <hildeamp@gmail.com>
2019-12-27 02:21:56 -0600

    :recycle: refactor validateJson middleware

--- src/common/middleware/validate-json.middleware.ts
+++ src/common/middleware/validate-json.middleware.ts
@@ -4,13 +4,19 @@ import { Request, Response } from 'express';
 @Injectable()
 export class ValidateJsonMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: Function): void {
-    if (req.headers['content-type'] !== 'application/json') {
-      res
-        .status(415)
-        .contentType('application/json')
-        .json({ message: 'JSON encoding required' });
-    } else {
+    const methods = ['get', 'delete'];
+
+    if (methods.includes(req.method.toLocaleLowerCase())) {
       next();
+    } else {
+      if (req.headers['content-type'] !== 'application/json') {
+        res
+          .status(415)
+          .contentType('application/json')
+          .json({ message: 'JSON encoding required' });
+      } else {
+        next();
+      }
     }
   }
 }
